be6764a2b29b71ae614fed4fb30176439febe11f
djGopinath <gopinath.dj619@gmail.com>
2025-03-13 16:38:20 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1 v-bind:id="headingId">Heading</h1>
+<button v-bind:disabled="isDisabled">Submit</button>
+<h2 class="underline">Underlined Text</h2>
+<h3 class=underline v-bind:class="status">Status</h3>
+<h4 v-bind:class="isPromoted && 'promoted'">Promoted Movies</h4>
+<h5 v-bind:class="isSoldOut ? 'sold-out':'new-movie'">Soldout? Movies</h5>
+<h6 v-bind:class="[isPromoted && 'promoted', isSoldOut && 'sold-out']">Array Conditioned class</h6>
+<h1 v-bind:class="{
+  promoted:isPromoted,
+  new:!isSoldOut,
+  'sold-out':isSoldOut
+}">object Conditioned Class</h1>
+<h2 v-bind:style="{
+  color:highlightendColor,fontSize:highlightendFont
+}">Inline Style</h2>
+<h3 v-bind:style="highlightendStyle">StyleObject</h3>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+     headingId:'header',
+     isDisabled:false,
+     status:'danger',
+     isPromoted:true,
+     isSoldOut:true,
+     highlightendColor:'orange',
+     highlightendFont:'20px',
+     highlightendStyle:{
+      color:'purple',
+      fontSize:'10px',
+      margin:'60px'
+     },
+     baseStyleObject:{
+      fontSize:'30px',
+      padding:'10px'
+     },
+     succesStyleObject:{
+        color:'pink',
+        border:'2px solid #2c3e50',
+        backgroundColor:'white'
+
+     }
+    };
+  },
 }
 </script>
 
@@ -22,5 +60,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline{
+  text-decoration: underline;
+}
+.promoted{
+  font-style: italic;
+}
+
+.new-movie{
+  color:green
+}
+
+.sold-out{
+  color: red;
 }
 </style>
