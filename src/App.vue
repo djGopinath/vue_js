8575d261f552b883c8cb6a96e198026e192f02b0
djGopinath <gopinath.dj619@gmail.com>
2025-03-14 10:34:25 +0530

    Added Conditional rendering v-if, v-else-if, v-else and v-show

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template>
+  <h2 v-if="num===0">The Number is Zero</h2>
+  <h2 v-else-if="num>0"> The Number is positive</h2>
+  <h2 v-else-if="num<0"> The Number is negative</h2>
+  <h2 v-else>The Number is not Zero</h2>
 
-<!-- shorthand property of v-bind is : with attributes  'v-bind:class' is converted to ':class' -->
-<h1 :id="headingId">Heading</h1>
-<button :disabled="isDisabled">Submit</button>
-<h2 class="underline">Underlined Text</h2>
-<h3 class=underline :class="status">Status</h3>
-<h4 :class="isPromoted && 'promoted'">Promoted Movies</h4>
-<h5 :class="isSoldOut ? 'sold-out':'new-movie'">Soldout? Movies</h5>
-<h6 :class="[isPromoted && 'promoted', isSoldOut && 'sold-out']">Array Conditioned class</h6>
+  <template v-if="isDisplay">
+    <h2>Hello </h2>
+    <h2> DEV</h2>
+    <h2>Vue</h2>
+  </template>
 
-<!-- style With condition -->
-<h1 :class="{
-  promoted:isPromoted,
-  new:!isSoldOut,
-  'sold-out':isSoldOut
-}">object Conditioned Class</h1>
-
-<!-- inline Style -->
-<h2 :style="{
-  color:highlightendColor,fontSize:highlightendFont
-}">Inline Style</h2>
-
-<!-- Style object -->
-<h3 :style="highlightendStyle">StyleObject</h3>
-
-<!-- Style object with Array -->
-<h4 :style="[succesStyleObject,dangerStyleObject,baseStyleObject]" :class="'promoted'">Style object with Array</h4>
+  <h3 v-show="showElement">Using v-show</h3>
+   <h3 v-if="showElement">Using v-if</h3>
 </template>
 
 <script>
@@ -34,38 +20,9 @@ export default {
   name: 'App',
   data(){
     return{
-     headingId:'header',
-     isDisabled:false,
-     status:'danger',
-     isPromoted:true,
-     isSoldOut:true,
-     highlightendColor:'orange',
-     highlightendFont:'20px',
-     highlightendStyle:{
-      color:'purple',
-      fontSize:'10px',
-      margin:'60px'
-     },
-
-     //style Object will pass into array
-     baseStyleObject:{
-      fontSize:'30px',
-      padding:'10px'
-     },
-
-     succesStyleObject:{
-        color:'pink',
-        border:'2px solid #2c3e50',
-        backgroundColor:'white',
-        padding:'20px'
-     },
-      dangerStyleObject:{
-        color:'red',
-        border:'3px solid #990b06',
-        backgroundColor:'#f57e7a',
-        padding:'20px'
-     }
-
+      num:0,
+      isDisplay:true,
+      showElement:false
     };
   },
 }
